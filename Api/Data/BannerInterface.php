845460c38010de466591b8eec550c86586355744
nbrabant <brabantnicolas59@gmail.com>
2021-02-11 15:43:09 +0100

    feat(Banner) : interface contract and implementation for Banner
    - see above

--- Api/Data/BannerInterface.php
+++ Api/Data/BannerInterface.php
@@ -8,5 +8,101 @@ namespace Nbrabant\Offers\Api\Data;
  */
 interface BannerInterface
 {
+    /**
+     * Get banner id.
+     *
+     * @return int|null
+     */
+    public function getId();
+
+    /**
+     * Get banner label.
+     *
+     * @return string
+     */
+    public function getLabel(): string;
+
+    /**
+     * Set banner label.
+     *
+     * @param string $label
+     * @return BannerInterface
+     */
+    public function setLabel(string $label): BannerInterface;
+
+    /**
+     * Get banner image source.
+     *
+     * @return string
+     */
+    public function getImageSrc(): string;
+
+    /**
+     * Set banner image source.
+     *
+     * @param string $imageSrc
+     * @return BannerInterface
+     */
+    public function setImageSrc(string $imageSrc): BannerInterface;
+
+    /**
+     * Get banner link.
+     *
+     * @return string
+     */
+    public function getLink(): string;
+
+    /**
+     * Set banner link.
+     *
+     * @param string $link
+     * @return BannerInterface
+     */
+    public function setLink(string $link): BannerInterface;
+
+    /**
+     * Get banner category Ids.
+     *
+     * @return string
+     */
+    public function getCategoryIds(): string;
+
+    /**
+     * Set banner category Ids.
+     *
+     * @param string $categoryIds
+     * @return BannerInterface
+     */
+    public function setCategoryIds(string $categoryIds): BannerInterface;
+
+    /**
+     * Get banner start publish date.
+     *
+     * @return \DateTimeInterface
+     */
+    public function getFromDate(): \DateTimeInterface;
+
+    /**
+     * Set banner start publish date.
+     *
+     * @param \DateTimeInterface $fromDate
+     * @return BannerInterface
+     */
+    public function setFromDate(\DateTimeInterface $fromDate): BannerInterface;
+
+    /**
+     * Get banner end publish date.
+     *
+     * @return \DateTimeInterface|null
+     */
+    public function getToDate(): ?\DateTimeInterface;
+
+    /**
+     * Set banner end publish date.
+     *
+     * @param \DateTimeInterface|null $toDate
+     * @return BannerInterface
+     */
+    public function setToDate(?\DateTimeInterface $toDate): BannerInterface;
 
 }
--- Model/Banner.php
+++ Model/Banner.php
@@ -28,7 +28,6 @@ class Banner extends AbstractModel implements BannerInterface, IdentityInterface
 
     protected function _construct()
     {
-        parent::_construct();
         $this->_init(BannerResourceModel::class);
         $this->setIdFieldName(BannerResourceInterface::BANNER_ID);
     }
@@ -41,4 +40,99 @@ class Banner extends AbstractModel implements BannerInterface, IdentityInterface
         return [self::CACHE_TAG . '_' . $this->getId()];
     }
 
+    /**
+     * @inheritDoc
+     */
+    public function getLabel(): string
+    {
+        return $this->getData(BannerResourceInterface::LABEL);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function setLabel(string $label): BannerInterface
+    {
+        return $this->setData(BannerResourceInterface::LABEL, $label);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getImageSrc(): string
+    {
+        return $this->getData(BannerResourceInterface::IMAGE_SRC);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function setImageSrc(string $imageSrc): BannerInterface
+    {
+        return $this->getData(BannerResourceInterface::IMAGE_SRC, $imageSrc);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getLink(): string
+    {
+        return $this->getData(BannerResourceInterface::LINK);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function setLink(string $link): BannerInterface
+    {
+        return $this->setData(BannerResourceInterface::LINK, $link);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getCategoryIds(): string
+    {
+        return $this->getData(BannerResourceInterface::LABEL);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function setCategoryIds(string $categoryIds): BannerInterface
+    {
+        return $this->setData(BannerResourceInterface::CATEGORY_IDS, $categoryIds);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getFromDate(): \DateTimeInterface
+    {
+        return $this->getData(BannerResourceInterface::LABEL);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function setFromDate(\DateTimeInterface $fromDate): BannerInterface
+    {
+        return $this->setData(BannerResourceInterface::FROM_DATE, $fromDate);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getToDate(): ?\DateTimeInterface
+    {
+        return $this->getData(BannerResourceInterface::LABEL);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function setToDate(?\DateTimeInterface $toDate): BannerInterface
+    {
+        return $this->setData(BannerResourceInterface::TO_DATE, $toDate);
+    }
 }
